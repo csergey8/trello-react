f224808f26f38545e740e258d4ec19f1719482ee
csergey8 <c.sergey@protonmail.com>
2019-12-10 10:33:20 +0200

    commit

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Card from '@material-ui/core/Card';
 import { Task } from '../Task';
 import Typography from '@material-ui/core/Typography';
-
+import styles from './Board.module.scss';
 
 interface IProps {
     board: {
@@ -11,11 +11,11 @@ interface IProps {
     };
 }
 
-export const Board: React.SFC<IProps> = ({ board }) => {
+export const Board: React.FC<IProps> = ({ board }: IProps) => {
     return (
         <React.Fragment>
-            <Card style={{ margin: '20px', width: 300}}>
-                <Typography color="textSecondary" variant="h5" gutterBottom>
+            <Card className={styles.card}>
+                <Typography color="textSecondary" variant="h6" gutterBottom>
                     {board.name}
                 </Typography>
                     {
